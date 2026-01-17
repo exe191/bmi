43c2c5f2fa287e87577501e89ba6a4a9ba87b6b5
exe191 <555dildin@gmail.com>
2026-01-17 19:05:37 +0300

    Сырая версия без gui.

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Привет")
+#Добавить gui
+print("Введите свое имя.")
+name = str(input())
+
+print("Введите свой возраст.")
+age = int(input())
+
+while age < 10:
+    print("Введите свой возраст еще раз.")
+    age = int(input())
+    if age >= 10:
+        break
+
+print("Введите свой вес.")
+wt = float(input())
+
+while wt > 300:
+    print("Введите свой вес еще раз.")
+    wt = float(input())
+    if wt <= 300:
+        break
+
+print("Введите свой рост.")
+height = float(input())
+
+while height > 3:
+    print("Введите свой рост еще раз.")
+    height = float(input())
+    if height <= 3:
+        break
+
+bmi = wt / height ** 2
+print(f"Имя: {name} Возраст: {age} Вес: {wt} Рост: {height}")
+print(f"Ваш лишний вес: {bmi}")
